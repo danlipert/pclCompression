6bd2d5516664b7e3a2124f422b68e88dcfae6b36
Jarrett Keifer <jarrettkeifer@gmail.com>
2015-01-20 16:36:55 -0800

    Initial Commit

---
+++ CMakeFiles/pcl-compression.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl-compression.dir/pcl-compression.cpp.o"
+  "pcl-compression.pdb"
+  "pcl-compression"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl-compression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
